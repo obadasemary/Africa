d5c0710e10c3bdc9624ecf4b4eb7029dea313111
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-21 18:30:14 +0300

    Add Swift Generics
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Bundle {
     
-    func decode(_ file: String) -> [CoverImage] {
+    func decode<T: Codable>(_ file: String) -> T {
         
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle")
@@ -21,7 +21,7 @@ extension Bundle {
         
         let decoder = JSONDecoder()
         
-        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle")
         }
         
