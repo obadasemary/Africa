002ad363510c9e3424925198e006577a0195bdae
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-21 17:52:43 +0300

    Update ContentView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Content")
-            .padding()
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+            .navigationBarTitle("Africa", displayMode: .large)
+        }
     }
 }
 
