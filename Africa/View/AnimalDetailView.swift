3ec1fa9e5936945b88e34005b89c067b5c0704c9
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-02 02:08:40 +0300

    Create AnimalDetailView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Africa/View/AnimalDetailView.swift
@@ -0,0 +1,71 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by Abdelrahman Mohamed on 2.10.2020.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            VStack(alignment: .center, spacing: 20) {
+                
+                // hero image
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // title
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // head line
+                
+                // gallery
+                
+                // facts
+                
+                // description
+                
+                // map
+                
+                // link
+                
+            }
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        NavigationView {
+            AnimalDetailView(animal: animals[0])
+        }
+    }
+}
