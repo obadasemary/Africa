efc46041d30f137770c1621a8b5c3679231ace00
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-02 03:14:03 +0300

    Create InsetFactView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Africa/View/InsetFactView.swift
@@ -0,0 +1,42 @@
+//
+//  InsetFactView.swift
+//  Africa
+//
+//  Created by Abdelrahman Mohamed on 2.10.2020.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        GroupBox {
+            TabView {
+                ForEach(animal.fact, id: \.self) { item in
+                    Text(item)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .frame(minHeight: 148, idealHeight: 168, maxHeight: 180)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+struct InsetFactView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        InsetFactView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
