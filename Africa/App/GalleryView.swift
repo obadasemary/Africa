7b3084385fc01890999e14b7aefdd8490035d005
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-03 23:02:38 +0300

    Update GalleryView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text("Gallery")
     }
 }
+
+// MARK: - PREVIEW
 
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
