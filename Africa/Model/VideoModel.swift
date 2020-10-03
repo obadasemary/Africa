922e3ee8a756ee7ab5d88cb9ad5a8e27d42f9cbe
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-03 22:43:43 +0300

    Create VideoModel.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,15 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Abdelrahman Mohamed on 3.10.2020.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let headline: String
+}
