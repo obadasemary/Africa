d3ae65c9236a0b872ddf528ec5d628fef5f3f0f6
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-09 00:05:00 +0300

    Update MapView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,15 +6,73 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     
     // MARK: - PROPERTIES
     
+    @State private var region: MKCoordinateRegion = {
+        
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     // MARK: - BODY
     
     var body: some View {
-        Text("Map")
+        
+        // MARK: - No1 BASIC MAP
+        // Map(coordinateRegion: $region)
+        
+        // MARK: - No2 ANNOTATIONS MAP
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+            
+            // MARK: - (A) PIN: OLD STYLE (always static)
+            // MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // MARK: - (B) MARKER: NEW STYLE (always static)
+            // MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // MARK: - (C) CUSTOM BASIC ANNOTATION: (it could be interactive)
+//            MapAnnotation(coordinate: item.location, anchorPoint: CGPoint(x: 0, y: 0)) {
+////                Image(item.image)
+//                ZStack {
+//                    Image("logo")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: 32, height: 32, alignment: .center)
+//                        .cornerRadius(6)
+//
+//                    Circle()
+//                        .foregroundColor(.clear)
+//                }
+//
+//
+//            }
+            MapAnnotation(coordinate: item.location) {
+
+                ZStack {
+                    
+                    Circle()
+                        .background(Color.accentColor)
+                        .foregroundColor(.accentColor)
+                        .cornerRadius(20)
+                    
+                    Image(item.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32, alignment: .center)
+                        .cornerRadius(16)
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
