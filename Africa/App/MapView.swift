d4a28ffb74aa0a87c92076a76ca470444476ec6f
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-10 02:47:11 +0300

    Add Compass
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -80,7 +80,50 @@ struct MapView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3, content: {
+                    
+                    HStack {
+                        
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        
+                        Spacer()
+                        
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        
+                        Spacer()
+                        
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                })
             })
+            .padding(.vertical, 12)
+            .padding(.horizontal, 16)
+            .background(
+                Color.black
+                    .cornerRadius(8)
+                    .opacity(0.6)
+            )
+            .padding()
+            .padding(.vertical, 25)
+            , alignment: .top
         )
         .edgesIgnoringSafeArea(.all)
     }
