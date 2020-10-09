e0460c103d6b2282a2140cf8aa62046ffa2b1e19
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-10 02:02:08 +0300

    Create MapAnnotationView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Africa/View/MapAnnotationView.swift
@@ -0,0 +1,27 @@
+//
+//  MapAnnotationView.swift
+//  Africa
+//
+//  Created by Abdelrahman Mohamed on 10.10.2020.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct MapAnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapAnnotationView()
+    }
+}
