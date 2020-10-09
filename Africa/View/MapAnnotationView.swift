cdfa652fad32ff26e3cc757a70d93efdc66620a9
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-10 02:18:27 +0300

    Update MapAnnotationView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- Africa/View/MapAnnotationView.swift
+++ Africa/View/MapAnnotationView.swift
@@ -11,17 +11,49 @@ struct MapAnnotationView: View {
     
     // MARK: - PROPERTIES
     
+    var location: NationalParkLocation
+    @State private var animation: Double = 0.0
+    
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            Circle()
+                .fill(Color.accentColor)
+                .frame(width: 54, height: 54, alignment: .center)
+            
+            Circle()
+                .stroke(Color.accentColor, lineWidth: 2)
+                .frame(width: 52, height: 52, alignment: .center)
+                .scaleEffect(1 + CGFloat(animation))
+                .opacity(1 - animation)
+            
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48, alignment: .center)
+                .clipShape(Circle())
+        }
+        .onAppear {
+            withAnimation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false)) {
+                
+                animation = 1
+            }
+        }
     }
 }
 
 // MARK: - PREVIEW
 
 struct MapAnnotationView_Previews: PreviewProvider {
+    
+    static var locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     static var previews: some View {
-        MapAnnotationView()
+        MapAnnotationView(location: locations[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
